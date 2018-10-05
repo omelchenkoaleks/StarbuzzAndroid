f64dbc6559c3d780fa72ee33e224f5f06d4ce03f
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-05 15:01:19 +0300

    Заполняем представление, полученными данными из массива по Id.

--- app/src/main/java/com/omelchenkoaleks/starbuzz/DrinkActivity.java
+++ app/src/main/java/com/omelchenkoaleks/starbuzz/DrinkActivity.java
@@ -2,6 +2,8 @@ package com.omelchenkoaleks.starbuzz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DrinkActivity extends Activity {
 
@@ -11,5 +13,22 @@ public class DrinkActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink);
+
+        // получаем напиток из данных интента
+        int drinkId = (Integer) getIntent().getExtras().get(EXTRA_DRINKID);
+        Drink drink = Drink.drinks[drinkId];
+
+        // заполняем название напитка
+        TextView name = findViewById(R.id.name);
+        name.setText(drink.getName());
+
+        // заполняем описание напитка
+        TextView description = findViewById(R.id.dimensions);
+        description.setText(drink.getDescription());
+
+        // заполняем изображение напитка
+        ImageView photo = findViewById(R.id.photo);
+        photo.setImageResource(drink.getImageResourceId());
+        photo.setContentDescription(drink.getName());
     }
 }
