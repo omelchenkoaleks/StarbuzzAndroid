80a586db50b99afcc4b4aec180575ea863e9b52f
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-13 18:33:49 +0300

    класс DrinkCategoryActivity переделан и теперь всю информацию берет из баззы данных - используется простой адаптер курсора так как в коде есть списковое представление

--- app/src/main/java/com/omelchenkoaleks/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/omelchenkoaleks/starbuzz/DrinkCategoryActivity.java
@@ -2,28 +2,60 @@ package com.omelchenkoaleks.starbuzz;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class DrinkCategoryActivity extends Activity {
+
+    // эти переменные добавляются для того, чтобы базу данных и курсор можно было закрыть в методе onDestroy()
+    private SQLiteDatabase mSQLiteDatabase;
+    private Cursor mCursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
 
-        // создаем адаптер массива для отображения данных из массива
-        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                Drink.drinks);
+        // массив не нужет - теперь база данных есть
+//        // создаем адаптер массива для отображения данных из массива
+//        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
+//                this,
+//                android.R.layout.simple_list_item_1,
+//                Drink.drinks);
 
         // адаптер массива связываем со списковым представлением методом setAdapter()
         ListView listDrinks = findViewById(R.id.list_drinks);
-        listDrinks.setAdapter(listAdapter);
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            // получить ссылку на базу данных
+            mSQLiteDatabase = starbuzzDatabaseHelper.getReadableDatabase();
+            // создать курсор
+            mCursor = mSQLiteDatabase.query("DRINK",
+                    new String[] {"_id", "NAME"},
+                    null, null, null, null, null);
+
+            // создаем адаптер курсора
+            SimpleCursorAdapter listAdapter = new SimpleCursorAdapter(this,
+                    android.R.layout.simple_list_item_1,
+                    mCursor,
+                    new String[] {"NAME"},
+                    new int[] {android.R.id.text1},
+                    0);
+
+            listDrinks.setAdapter(listAdapter);
+
+        } catch (SQLiteException ex) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
 
         // создаем слушателя
         AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
@@ -38,5 +70,13 @@ public class DrinkCategoryActivity extends Activity {
         };
         // назначить слушатель для спискового представления
         listDrinks.setOnItemClickListener(itemClickListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // закрываем курсор и базу данных
+        mCursor.close();
+        mSQLiteDatabase.close();
     }
 }
